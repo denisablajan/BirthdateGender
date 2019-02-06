f5c612d61b3309645f8451e99f239fc59f533f97
Denisa Blăjan <deniblajan93@gmail.com>
2019-02-06 17:37:06 +0200

    Improving code

--- BirthdateGender/Program.cs
+++ BirthdateGender/Program.cs
@@ -10,58 +10,96 @@ namespace BirthdateGender
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your birth day, month and year : ");
-
-            char[] separators = { ' ', ',', '.', '-', ':', '\t' };
-            string[] enteredDate = Console.ReadLine().Split(separators);
-            int day, month, year;
-
-            day = int.Parse(enteredDate[0]);
-            month = int.Parse(enteredDate[1]);
-            year = int.Parse(enteredDate[2]);
-            DateCheck(ref day, ref month, ref year);
-
-            DateTime birthdate = new DateTime(year, month, day);
-            int age = DateTime.Now.Year - birthdate.Year;
-
+            Console.WriteLine("Enter your birthdate: ");
+            DateTime birthdate = ReadBirthDate();
+            int age = GetAge(birthdate);
             Console.WriteLine("If your birthdate is " + birthdate.ToString("dd-MM-yyyy") + " you are " + age + " years old.");
 
             Console.WriteLine("Enter the first letter of your gender: ");
             GenderCheck(age);
         }
 
-        static void DateCheck(ref int day, ref int month, ref int year)
+        static int GetAge(DateTime birthdate)
         {
-        dayLabel:
-            if (day > 31 || day <= 0)
-            {
-                Console.WriteLine("Invalid day! Enter your birth day.");
-                day = int.Parse(Console.ReadLine());
-                goto dayLabel;
-            }
-
-        monthLabel:
-            if (month > 12 || month <= 0)
-            {
-                Console.WriteLine("Invalid month! Enter your birth month.");
-                month = int.Parse(Console.ReadLine());
-                goto monthLabel;
-            }
-
-        yearLabel:
-            if (year > DateTime.Now.Year || year <= 0)
-            {
-                Console.WriteLine("Invalid year! Enter your birth year.");
-                year = int.Parse(Console.ReadLine());
-                goto yearLabel;
-            }
+            int years = DateTime.Now.Year - birthdate.Year;
+            if ((birthdate.Month > DateTime.Now.Month) || (birthdate.Day > DateTime.Now.Day))
+                years--;
+            return years;
         }
+        static DateTime ReadBirthDate()
+        {
 
+            Console.WriteLine("Day: ");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Month: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            if (!IsValid(day, month, year))
+            {
+                do
+                {
+                    Console.WriteLine("Invalid date. Try again.");
+                    Console.WriteLine("Day: ");
+                    day = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Month: ");
+                    month = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Year: ");
+                    year = int.Parse(Console.ReadLine());
+                } while (!IsValid(day, month, year));
+            }
+
+            DateTime date = new DateTime(year, month, day);
+            return date;
+        }
+        static bool IsLeap(int year)
+        {
+            return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+        }
+        static bool IsValid(int day, int month, int year)
+        {
+            //check if date is not out of range
+            if (year > DateTime.Now.Year || year <= 0)
+                return false;
+            if (month < 1 || month > 12)
+                return false;
+            if (day < 1 || day > 31)
+                return false;
+            //check for April, June, September, November
+            if (month == 4 || month == 6 || month == 9 || month == 10)
+                return day <= 30;
+            //check Leap Day / Leap Year
+            if(month == 2)
+            {
+                if (IsLeap(year))
+                    return day <= 29;
+                else
+                    return day <= 28;
+            }
+
+            return true;
+        }
+        static bool ValidGender(char genderInput)
+        {
+            if ((genderInput != 'F') && (genderInput != 'f') && (genderInput != 'M') && (genderInput != 'm'))
+                return false;
+            return true;
+        }
         static void GenderCheck(int age)
         {
-        genderLabel:
+
             char genderInput = char.Parse(Console.ReadLine());
             Gender? genderOutput;
+
+            if (!ValidGender(genderInput))
+            {
+                do
+                {
+                    Console.WriteLine("Try again:");
+                    genderInput = char.Parse(Console.ReadLine());
+                } while (!ValidGender(genderInput));
+            }
 
             if (genderInput == 'F' || genderInput == 'f')
             {
@@ -77,14 +115,7 @@ namespace BirthdateGender
                     Console.WriteLine("You will retire at age 65.");
                 else Console.WriteLine("You are retired.");
             }
-            else
-            {
-                genderOutput = null;
-                Console.WriteLine("Invalid character for gender, enter the letter of your gender again!");
-                goto genderLabel;
-            }
         }
-
         public enum Gender { Female, Male }
     }
 }
